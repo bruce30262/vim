361fe4088c07caf8bf08ac07795ffd55adf0dadb
Bruce Chen <bruce30262@pm.me>
2023-01-22 00:11:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 sudo apt update
 
@@ -12,8 +12,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installing vim plugins..."
+set +e # disable -e because the followings will return 1 even if all plugins have installed successfully
 vim -es -u ~/.vim/.vimrc -i NONE -c "PlugInstall" -c "qa"
 
+set -e
 echo "Creating .vimrc symlink..."
 ln -sf ~/.vim/.vimrc ~/.vimrc
 
